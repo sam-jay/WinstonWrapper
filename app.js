fe869b814a6296153020dd3bd71eb25dece7252a
skg2122 <skg2122@columbia.edu>
2015-04-06 15:31:07 -0400

    Basic express

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 (function() {
   'use strict';
   var config = require('./config');
+
+  var express = require('express');
+  var app = express();
+
+  app.get('/',function(req,res){
+   res.send();
+  });
+
+  app.listen(config.port);
+
 })()
