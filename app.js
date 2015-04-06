d63207cc72f8f8db1d0b1143eaff26aeb1c5495f
skg2122 <skg2122@columbia.edu>
2015-04-06 16:20:31 -0400

    Editing SNS

--- app.js
+++ app.js
@@ -4,6 +4,8 @@
 
   var express = require('express');
   var parser = require('body-parser');
+  var aws = require('aws-sdk');
+
   var app = express();
   app.use(parser.json());
 
@@ -13,7 +15,7 @@
   var SNS_KEY_ID = process.env['SNS_KEY_ID'],
   SNS_ACCESS_KEY = process.env['SNS_ACCESS_KEY'];
 
-  var sns = new AWS.SNS({
+  var sns = new aws.SNS({
     region: 'us-east-1',
     accessKeyId: SNS_ACCESS_KEY,
     secretAccessKey: SNS_KEY_ID
@@ -22,7 +24,7 @@
   app.post('/logger/event', function(req, res){
     if (!req.body) return res.sendStatus(400);
     console.log(req.body);
-    sns.publish({topic: "winston", message: req.body.data}, function(err, res){
+    sns.publish({Message: req.body.data}, function(err, res){
         if (err) console.log(err, err.stack);
         else console.log(req.body.data);
   });
