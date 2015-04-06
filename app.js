e3cd32cca8ca39c2c18612e3a7e7766a8a6e207b
skg2122 <skg2122@columbia.edu>
2015-04-06 16:10:53 -0400

    Update

--- app.js
+++ app.js
@@ -6,17 +6,6 @@
   var parser = require('body-parser');
   var app = express();
   app.use(parser.json());
-<<<<<<< HEAD
-
-  app.listen(config.port);
-  console.log("Listening on port: " + config.port);
-
-  app.post('/logger/event', function(req, res) {
-  	if (!req.body) return res.sendStatus(400);
-  	console.log(req.body);
-  	res.send('priority ' + req.body.priority + ' data: \n' + req.body.data);
-  });
-=======
 
   app.listen(config.port);
   console.log("Listening on port: " + config.port);
@@ -34,10 +23,9 @@
     if (!req.body) return res.sendStatus(400);
     console.log(req.body);
     sns.publish({topic: "winston", message: req.body.data}, function(err, res){
-	if (err) console.log(err, err.stack);
-	else console.log(req.body.data);
+        if (err) console.log(err, err.stack);
+        else console.log(req.body.data);
   });
 });
->>>>>>> f6e1aafed6fa2c38223533ebd1c9c71a40fb21a4
 
 })()
