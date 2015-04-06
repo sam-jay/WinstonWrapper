022bdfb930dfa5b16084343a9466a66f74ec5f3b
skg2122 <skg2122@columbia.edu>
2015-04-06 17:10:45 -0400

    Finished with SQS and SNS

--- app.js
+++ app.js
@@ -18,17 +18,36 @@
         secretAccessKey: '***'
   });
 
+  var sqs = new aws.SQS({
+        region: 'us-east-1',
+        accessKeyId: '***',
+        secretAccessKey: '***',
+  });
+
   app.post('/logger/event', function(req, res){
     if (!req.body) return res.sendStatus(400);
     console.log(req.body);
+
     var params = {
-        TargetArn: '***',
+        TargetArn: 'arn:aws:sns:us-east-1:216982984213:winston',
 	Message: req.body.data};
-  
-  sns.publish(params, function(err, data){
-        if (err) console.log(err, err.stack);
-        else console.log(data);
-  });
+
+  if (req.body.priority === 'high'){
+	  sns.publish(params, function(err, data){
+		if (err) console.log(err, err.stack);
+		else console.log(data);
+	  });
+  }
+  else{
+	var params = {
+	QueueUrl: 'https://sqs.us-east-1.amazonaws.com/216982984213/winston',
+	MessageBody: req.body.data};
+
+	sqs.sendMessage(params, function(err,data){
+		if(!err) console.log('Message sent.');
+		else console.log(err, err.stack);
+	});
+  }
 });
 
 })()
